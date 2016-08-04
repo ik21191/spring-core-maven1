7e5a0c7b4eb90bde41efc6616e72d337bcd5d86c
Imran Khan <imran.khan@adi-mps.com>
2016-08-04 16:29:29 +0530

    [v2]: @Qualifier Annotation is used.

--- src/main/java/com/mypack/service/StudentServiceImpl.java
+++ src/main/java/com/mypack/service/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mypack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.mypack.dao.StudentDAO;
@@ -9,7 +10,11 @@ import com.mypack.dto.StudentDTO;
 @Service ("studentService")
 public class StudentServiceImpl implements StudentService {
 
+	public StudentServiceImpl() {
+		System.out.println("Constructor StudentServiceImpl called..............");
+	}
 	@Autowired
+	@Qualifier("studentDao1")//This Annotation is used when more than one implementation of an interface is given
 	StudentDAO dao;
 	public StudentDTO createNewStudent() {
 		return dao.createNewStudent();
