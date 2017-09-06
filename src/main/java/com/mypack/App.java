efb90edd44b00aa54652a78a8e1c77e40e0388b3
Imran Khan <imran.khan@adi-mps.com>
2017-09-06 12:45:29 +0530

    [v4]:formatting

--- src/main/java/com/mypack/App.java
+++ src/main/java/com/mypack/App.java
@@ -7,18 +7,17 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-public class App 
-{
-    public static void main( String[] args ) {
-    	Resource resource = new ClassPathResource("Spring-Module.xml");
-    	BeanFactory factory = new XmlBeanFactory(resource);//Core IOC container
-    	//ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");//Advance IOC container
-    	User obj = (User) factory.getBean("userBean");
-    	System.out.println(obj.getName());
-    	System.out.println(obj.getRoll());
-    	//System.out.println(obj.getCource().getId()+"   "+obj.getCource().getName());
-    	System.out.println("List: " + obj.getList());
-    	System.out.println("List: " + obj.getSet());
-    	System.out.println("List: " + obj.getMap());
-    }
+public class App {
+	public static void main(String[] args) {
+		Resource resource = new ClassPathResource("Spring-Module.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);// Core IOC container
+		// ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");//Advance IOC container
+		User obj = (User) factory.getBean("userBean");
+		System.out.println(obj.getName());
+		System.out.println(obj.getRoll());
+		// System.out.println(obj.getCource().getId()+" "+obj.getCource().getName());
+		System.out.println("List: " + obj.getList());
+		System.out.println("List: " + obj.getSet());
+		System.out.println("List: " + obj.getMap());
+	}
 }
