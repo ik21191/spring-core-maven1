970bc2eb529a765813079a7213c61b406948a5f8
Imran Khan <imran.khan@adi-mps.com>
2017-12-24 23:20:00 +0530

    [v4]:look-up-method concept added

--- src/main/java/com/mypack/AutoWireTest.java
+++ src/main/java/com/mypack/AutoWireTest.java
@@ -1,6 +1,5 @@
 package com.mypack;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mypack.beans.A;
@@ -9,9 +8,18 @@ import com.mypack.beans.A;
 
 public class AutoWireTest 
 {
-    public static void main( String[] args ){
-    	ApplicationContext context = new ClassPathXmlApplicationContext("auto-wired.xml");
-    	A a = (A) context.getBean("a");
-    	a.getB().display();
+    public static void main( String[] args ) {
+    	ClassPathXmlApplicationContext context = null;
+    	//ApplicationContext context = null;
+    	try {
+    		context = new ClassPathXmlApplicationContext("auto-wired.xml");
+        	A a = (A) context.getBean("a");
+        	a.getB().display();
+    	} catch(Exception e) {
+    		System.out.println(e);
+    	} finally {
+    		context.close();
+    	}
+    	
     }
 }
--- src/main/java/com/mypack/spring/method/lookup/PizzaShop.java
+++ src/main/java/com/mypack/spring/method/lookup/PizzaShop.java
@@ -1,5 +1,8 @@
 package com.mypack.spring.method.lookup;
 
 public abstract class PizzaShop {
+	public PizzaShop() {
+		System.out.println("PizzaShop is created.");
+	}
 	public abstract Pizza makePizza();
 }
